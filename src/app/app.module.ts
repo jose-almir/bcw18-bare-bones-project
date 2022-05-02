84522537552bc06bc8241b4b4759240ffc9ef139
jrcodev <jr.cod.dev@gmail.com>
2022-05-02 16:49:42 -0300

    0.2 - Componentes customizados e interpolação de strings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { ProdutoComponent } from './components/produto/produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    ProdutosComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule,
